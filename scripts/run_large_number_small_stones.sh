f0f5f18317977e311a8120537b8c743b1b669621
April Wright <wright.aprilm@gmail.com>
2025-08-17 08:09:53 -0400

    update run script so it renames properly

---
+++ scripts/run_large_number_small_stones.sh
@@ -0,0 +1,8 @@
+for file in sets/*.clean;
+    do
+        echo $file
+        cp $file sets/file.nex;
+        rb scripts/large_number_small_stones.Rev;
+        mv output/ss10000 output/$file;
+    done
+
